5c82e2946cf498498d982db61264e6e6bc5c65ff
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2020-04-10 12:01:16 -0300

    firstCommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace buying_order_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
