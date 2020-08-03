0f5f7af8f9bd915ea135fbe19a78f71c48ed0297
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2020-08-02 22:32:05 -0300

    adicionado dockerfile

--- buying_order_server/build-image.bat
+++ buying_order_server/build-image.bat
@@ -1,3 +1,4 @@
 @echo off
 set /p version="Enter version: "
+dotnet publish --force --runtime linux-x64 --self-contained true --output ./dist
 docker build --no-cache -t buying-order-agent:%version%
