9613542b81c64095b853ffbee36724cb1c71c603
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2020-08-02 23:28:32 -0300

    ajustado docker

--- buying_order_server/build-image.bat
+++ buying_order_server/build-image.bat
@@ -1,4 +1,5 @@
 @echo off
 set /p version="Enter version: "
+rm -rdf ./dist
 dotnet publish --force --runtime linux-x64 --self-contained true --output ./dist
-docker build --no-cache -t buying-order-agent:%version%
+docker build . --no-cache -t buying-order-server:%version% -t buying-order-server:latest
